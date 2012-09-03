ea90993216390b7a42825e6999abdce7e2352ecb
Joseph Barrozo <JABarrozO@gmail.com>
2012-09-04 00:18:14 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TenThousandInsertQueries::Application.config.secret_token = '120060d21ef7fb317e268aca05fb3a4d69ccd6433c600a2e731d298999513a7e420b61f5ae2ffbaf34eefccf69ad33c2be86a6151c3858082da8a95d587c5098'
